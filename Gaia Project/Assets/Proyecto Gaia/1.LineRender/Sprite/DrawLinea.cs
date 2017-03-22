1686bc28720ecfe5861af9931e7e471fa1a9d0d4
Moon Antonio <pinchodev91@gmail.com>
2017-03-23 00:33:34 +0100

    Fix remote

---
+++ Gaia Project/Assets/Proyecto Gaia/1.LineRender/Sprite/DrawLinea.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// DrawLinea.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho
+{
+	public class DrawLinea : MonoBehaviour 
+	{
+
+	}
+}

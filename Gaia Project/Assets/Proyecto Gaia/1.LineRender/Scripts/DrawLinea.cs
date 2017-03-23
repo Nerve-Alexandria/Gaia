bbfd82048c9aaf9b7eec32526a08750a6166c36a
Moon Antonio <pinchodev91@gmail.com>
2017-03-23 01:00:39 +0100

    Implementando estructura DrawLinea.cs

---
+++ Gaia Project/Assets/Proyecto Gaia/1.LineRender/Scripts/DrawLinea.cs
@@ -0,0 +1,48 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// DrawLinea.cs (23/03/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Dibuja una linea en la direccion dada						\\
+// Fecha Mod:		23/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections.Generic;
+#endregion
+
+namespace MoonPincho
+{
+	/// <summary>
+	/// <para>Dibuja una linea en la direccion dada.</para>
+	/// </summary>
+	public class DrawLinea : MonoBehaviour 
+	{
+		#region Variables Publicas
+		public LineRenderer lineRenderer;
+		public EdgeCollider2D col;
+		public List<Vector2> vectores;
+		#endregion
+
+		#region Actualizadores
+
+		public void UpdateLinea(Vector2 vector)
+		{
+			if (vectores == null)
+			{
+				vectores = new List<Vector2>();
+				SetVectores(vector);
+				return;
+			}
+		}
+		#endregion
+
+		#region API	
+		private void SetVectores(Vector2 vector)
+		{
+			vectores.Add(vector);
+		}
+		#endregion
+	}
+}

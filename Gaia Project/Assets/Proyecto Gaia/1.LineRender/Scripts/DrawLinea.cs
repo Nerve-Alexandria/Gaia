0099202ab7f4e01265dc59fe164129f26015c7f4
Moon Antonio <pinchodev91@gmail.com>
2017-03-23 01:08:05 +0100

    Comentado DrawLinea.cs

--- Gaia Project/Assets/Proyecto Gaia/1.LineRender/Scripts/DrawLinea.cs
+++ Gaia Project/Assets/Proyecto Gaia/1.LineRender/Scripts/DrawLinea.cs
@@ -20,28 +20,56 @@ namespace MoonPincho
 	public class DrawLinea : MonoBehaviour 
 	{
 		#region Variables Publicas
-		public LineRenderer lineRenderer;
-		public EdgeCollider2D col;
-		public List<Vector2> vectores;
+		/// <summary>
+		/// <para>Line Renderer de la linea.</para>
+		/// </summary>
+		public LineRenderer lineRenderer;										// Line Renderer de la linea
+		/// <summary>
+		/// <para>Collider de la linea.</para>
+		/// </summary>
+		public EdgeCollider2D edgeCol;											// Collider de la linea
+		/// <summary>
+		/// <para>Lista de vectores de la linea.</para>
+		/// </summary>
+		public List<Vector2> vectores;											// Lista de vectores de la linea
 		#endregion
 
 		#region Actualizadores
-
-		public void UpdateLinea(Vector2 vector)
+		/// <summary>
+		/// <para>Actualiza la linea con los vectores dados.</para>
+		/// </summary>
+		/// <param name="vector">Vector dado.</param>
+		public void UpdateLinea(Vector2 vector)// Actualiza la linea con los vectores dados
 		{
+			// Si no hay vectores
 			if (vectores == null)
 			{
+				// Obtener los vectores
 				vectores = new List<Vector2>();
 				SetVectores(vector);
 				return;
 			}
+
+			// Posicion raton & touch
 		}
 		#endregion
 
 		#region API	
-		private void SetVectores(Vector2 vector)
+		/// <summary>
+		/// <para>Fija los vectores dados.</para>
+		/// </summary>
+		/// <param name="vector">Vector dado.</param>
+		private void SetVectores(Vector2 vector)// Fija los vectores dados
 		{
+			// Agrega el vector a la lista de vectores
 			vectores.Add(vector);
+
+			// Actualiza el numero de posiciones
+			lineRenderer.numPositions = vectores.Count;
+
+			// Fija la posicion de la linea
+			lineRenderer.SetPosition(vectores.Count - 1, vector);
+			edgeCol.points = vectores.ToArray();
 		}
 		#endregion
 	}

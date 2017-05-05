189683e4c7ef5cc6f1ff0fb1a8b1c6a7dd08b795
Moon Antonio <pinchodev91@gmail.com>
2017-05-06 00:41:25 +0200

    Add obstaculos y modificada la iluminacion

---
+++ Gaia Project/Assets/Proyecto Gaia/16.Sigilo/Scripts/Heroe.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Heroe.cs (06/05/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Controller del heroe										\\
+// Fecha Mod:		06/05/2017													\\
+// Ultima Mod:		Version inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Sigilo
+{
+	/// <summary>
+	/// <para>Controller del heroe</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Sigilo/Heroe")]
+	public class Heroe : MonoBehaviour 
+	{
+
+	}
+}

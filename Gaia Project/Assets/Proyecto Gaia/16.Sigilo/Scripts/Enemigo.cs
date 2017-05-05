58c8ab00ae63f7914314974e2d8c5c9b9817a2f2
Moon Antonio <pinchodev91@gmail.com>
2017-05-05 22:41:06 +0200

    Mostrar la ruta a seguir

--- Gaia Project/Assets/Proyecto Gaia/16.Sigilo/Scripts/Enemigo.cs
+++ Gaia Project/Assets/Proyecto Gaia/16.Sigilo/Scripts/Enemigo.cs
@@ -19,6 +19,34 @@ namespace MoonAntonio.Sigilo
 	[AddComponentMenu("MoonAntonio/Sigilo/Enemigo")]
 	public class Enemigo : MonoBehaviour 
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Ruta que seguir</para>
+		/// </summary>
+		public Transform path;                                          // Ruta que seguir
+		#endregion
 
+		#region Metodos
+		/// <summary>
+		/// <para>Dibujar gizmos</para>
+		/// </summary>
+		private void OnDrawGizmos()// Dibujar gizmos
+		{
+			// Si la ruta no es null
+			if (path != null)
+			{
+				Vector3 posInicial = path.GetChild(0).position;
+				Vector3 posAnterior = posInicial;
+
+				// Recorrer todos los waypoints y dibujarlos
+				foreach (Transform way in path)
+				{
+					Gizmos.DrawSphere(way.position, 0.3f);
+					Gizmos.DrawLine(posAnterior, way.position);
+					posAnterior = way.position;
+				}
+			}
+		}
+		#endregion
 	}
 }

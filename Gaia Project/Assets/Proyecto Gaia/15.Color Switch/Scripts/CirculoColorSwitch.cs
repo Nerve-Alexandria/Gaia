19b191c130f689460beea0288c6afd6079276c09
Moon Antonio <pinchodev91@gmail.com>
2017-05-05 20:24:58 +0200

    Add funcionalidad al controller

--- Gaia Project/Assets/Proyecto Gaia/15.Color Switch/Scripts/CirculoColorSwitch.cs
+++ Gaia Project/Assets/Proyecto Gaia/15.Color Switch/Scripts/CirculoColorSwitch.cs
@@ -22,7 +22,32 @@ namespace MoonAntonio.ColorSwitch
 		/// <summary>
 		/// <para>Fuerza del impulso del circulo.</para>
 		/// </summary>
-		public float fuerza = 0.0f;												// Fuerza del impulso del circulo
+		public float fuerza = 0.0f;                                             // Fuerza del impulso del circulo
+		/// <summary>
+		/// <para>RigidBody del circulo.</para>
+		/// </summary>
+		public Rigidbody2D rb;                                                  // RigidBody del circulo
+		#endregion
+
+		#region Actualizadores
+		/// <summary>
+		/// <para>Actualizador de CirculoColorSwitch.</para>
+		/// </summary>
+		private void Update()// Actualizador de CirculoColorSwitch
+		{
+			// Cuando damos click, damos impulso
+			if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)) Impulso();
+		}
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Da un impulso al circulo.</para>
+		/// </summary>
+		private void Impulso()// Da un impulso al circulo
+		{
+			rb.velocity = Vector2.up * fuerza;
+		}
 		#endregion
 	}
 }

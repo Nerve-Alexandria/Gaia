d7635e741b05b8dd7d520b3c24c2fe91295fdad0
Moon Antonio <pinchodev91@gmail.com>
2017-05-05 22:35:05 +0200

    Add enemigo

---
+++ Gaia Project/Assets/Proyecto Gaia/16.Sigilo/Scripts/Enemigo.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Enemigo.cs (05/05/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Controller del enemigo										\\
+// Fecha Mod:		05/05/2017													\\
+// Ultima Mod:		Version inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Sigilo
+{
+	/// <summary>
+	/// <para>Controller del enemigo</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Sigilo/Enemigo")]
+	public class Enemigo : MonoBehaviour 
+	{
+
+	}
+}

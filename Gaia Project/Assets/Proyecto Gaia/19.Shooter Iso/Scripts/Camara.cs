12426acceb350a16b4b2b7fc3f4f9ca17afef09d
Moon Antonio <pinchodev91@gmail.com>
2017-06-06 20:31:26 +0200

    Add Scripts Necesarios

---
+++ Gaia Project/Assets/Proyecto Gaia/19.Shooter Iso/Scripts/Camara.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Camara.cs (06/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control de la camara										\\
+// Fecha Mod:		06/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.ShooterIso
+{
+	/// <summary>
+	/// <para>Control de la camara</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/ShooterIso/Camara")]
+	public class Camara : MonoBehaviour 
+	{
+
+	}
+}
---
+++ Gaia Project/Assets/Proyecto Gaia/19.Shooter Iso/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Player.cs (06/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control del player											\\
+// Fecha Mod:		06/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.ShooterIso
+{
+	/// <summary>
+	/// <para>Control del player</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/ShooterIso/Player")]
+	public class Player : MonoBehaviour 
+	{
+
+	}
+}

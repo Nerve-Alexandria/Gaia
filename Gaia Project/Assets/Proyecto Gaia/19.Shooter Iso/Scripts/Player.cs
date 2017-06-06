607f7695b3d28942a7c0550f8e62b85e413aa4ce
Moon Antonio <pinchodev91@gmail.com>
2017-06-06 21:03:41 +0200

    Rotacion del player

--- Gaia Project/Assets/Proyecto Gaia/19.Shooter Iso/Scripts/Player.cs
+++ Gaia Project/Assets/Proyecto Gaia/19.Shooter Iso/Scripts/Player.cs
@@ -20,11 +20,50 @@ namespace MoonAntonio.ShooterIso
 	public class Player : MonoBehaviour 
 	{
 		#region Variables
-
+		/// <summary>
+		/// <para>Velocidad de movimiento del <see cref="Player"/>.</para>
+		/// </summary>
+		public float vel = 0.0f;                                            // Velocidad de movimiento del Player
+		/// <summary>
+		/// <para>Prefab de la camara de la escena.</para>
+		/// </summary>
+		public GameObject camara;                                           // Prefab de la camara de la escena
+		/// <summary>
+		/// <para>OffSet del player</para>
+		/// </summary>
+		public float offSet = 0.0f;											// OffSet del player
 		#endregion
 
-		#region Metodos
+		#region Actualizadores
+		/// <summary>
+		/// <para>Actualizador de <see cref="Player"/>.</para>
+		/// </summary>
+		private void Update()// Actualizador de Player
+		{
+			// Creamos un plano encima del player
+			Plane playerP = new Plane(Vector3.up, this.transform.position);
 
+			// Tiramos un rayo desde la posicion de la camara a la posicion del raton
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			// Creamos una variable para comprobar la distancia del hit
+			float hitDist = 0.0f;
+
+			// Comprobamos el raycast
+			if (playerP.Raycast(ray, out hitDist))
+			{
+				// Asignamos las variables de posicion y rotacion
+				Vector3 targetP = ray.GetPoint(hitDist);
+				Quaternion targetR = Quaternion.LookRotation(targetP - this.transform.position);
+
+				// Freezeamos x/z
+				targetR.x = 0;
+				targetR.z = 0;
+
+				// Interpolamos la rotacion con un tiempo
+				this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetR, offSet * Time.deltaTime);
+			}
+		}
 		#endregion
 	}
 }

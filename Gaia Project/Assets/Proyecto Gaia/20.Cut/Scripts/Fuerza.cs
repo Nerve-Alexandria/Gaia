01c00b6142e542841c68c5c132c3214a89978135
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 07:17:26 +0200

    Fix Fuerza.cs

--- Gaia Project/Assets/Proyecto Gaia/20.Cut/Scripts/Fuerza.cs
+++ Gaia Project/Assets/Proyecto Gaia/20.Cut/Scripts/Fuerza.cs
@@ -1,18 +1,22 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// Fuerza.cs (00/00/0000)													\\
+// Fuerza.cs (10/06/2017)														\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Control de la fuerza										\\
+// Fecha Mod:		10/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
 using UnityEngine;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.Cut
 {
+	/// <summary>
+	/// <para>Control de la fuerza</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Cut/Fuerza")]
 	public class Fuerza : MonoBehaviour 
 	{
 

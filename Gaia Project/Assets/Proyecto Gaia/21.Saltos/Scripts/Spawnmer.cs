4485b7de34c357409205f5b9b0cd583e67f1958b
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 08:50:25 +0200

    Fix Spawnmer.cs

--- Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Spawnmer.cs
+++ Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Spawnmer.cs
@@ -1,18 +1,22 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// Spawnmer.cs (00/00/0000)													\\
+// Spawnmer.cs (10/06/2017)														\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Manager de Spawns del enemigo								\\
+// Fecha Mod:		10/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
 using UnityEngine;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.Saltos
 {
+	/// <summary>
+	/// <para>Manager de Spawns del enemigo</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Saltos/Spawnmer")]
 	public class Spawnmer : MonoBehaviour 
 	{
 

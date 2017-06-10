a1639bb28e2a6b93eeabb95f03ee04cc9c787582
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 07:01:26 +0200

    Creado Punto.cs para controlar el punto en escena

---
+++ Gaia Project/Assets/Proyecto Gaia/20.Cut/Scripts/Punto.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Punto.cs (10/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control del punto											\\
+// Fecha Mod:		10/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Cut
+{
+	/// <summary>
+	/// <para>Control del punto</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Cut/Punto")]
+	public class Punto : MonoBehaviour 
+	{
+
+	}
+}

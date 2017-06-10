d38152e16ed329acf918e783da3d734dac28881e
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 08:35:35 +0200

    Fix Enemigo.cs

--- Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Enemigo.cs
+++ Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Enemigo.cs
@@ -1,18 +1,22 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// Enemigo.cs (00/00/0000)													\\
+// Enemigo.cs (10/06/2017)														\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Control del Enemigo											\\
+// Fecha Mod:		10/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
 using UnityEngine;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.Saltos
 {
+	/// <summary>
+	/// <para>Control del Enemigo</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Saltos/Enemigo")]
 	public class Enemigo : MonoBehaviour 
 	{
 

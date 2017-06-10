02d59ecff66b622888764dde5bb52743659e891d
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 08:24:27 +0200

    Add Jugador.cs

---
+++ Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Jugador.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Jugador.cs (10/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control del Jugador											\\
+// Fecha Mod:		10/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Saltos
+{
+	/// <summary>
+	/// <para>Control del Jugador</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Saltos/Jugador")]
+	public class Jugador : MonoBehaviour 
+	{
+	}
+}

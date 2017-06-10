f31e7161fc0b6e3fc70e5bebd6cde708d1557c6a
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 07:06:31 +0200

    Generador de las lineas

--- Gaia Project/Assets/Proyecto Gaia/20.Cut/Scripts/Punto.cs
+++ Gaia Project/Assets/Proyecto Gaia/20.Cut/Scripts/Punto.cs
@@ -19,6 +19,43 @@ namespace MoonAntonio.Cut
 	[AddComponentMenu("MoonAntonio/Cut/Punto")]
 	public class Punto : MonoBehaviour 
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>El Hinge del hook.</para>
+		/// </summary>
+		public HingeJoint2D hook;										// El Hinge del hook
+		/// <summary>
+		/// <para>El prefab de la union.</para>
+		/// </summary>
+		public GameObject unionPrefab;									// El prefab de la union
+		/// <summary>
+		/// <para>Cantidad de uniones que tendra.</para>
+		/// </summary>
+		public int unionesMax = 0;                                      // Cantidad de uniones que tendra
+		#endregion
 
+		#region Inicializadores
+		/// <summary>
+		/// <para>Inicializador de <see cref="Punto"/>.</para>
+		/// </summary>
+		private void Start()// Inicializador de Punto
+		{
+			// Generar las lineas
+			GenerarLineas();
+		}
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Genera las lineas del punto.</para>
+		/// </summary>
+		private void GenerarLineas()// Genera las lineas del punto
+		{
+			for (int n = 0; n < unionesMax; n++)
+			{
+				Instantiate(unionPrefab, this.transform);
+			}
+		}
+		#endregion
 	}
 }

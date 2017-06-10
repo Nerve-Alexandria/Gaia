0e4f63734ad78a6acfd607df8e9454ff87ae8129
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 08:29:30 +0200

    Add control de movimiento del jugador

--- Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Jugador.cs
+++ Gaia Project/Assets/Proyecto Gaia/21.Saltos/Scripts/Jugador.cs
@@ -19,5 +19,42 @@ namespace MoonAntonio.Saltos
 	[AddComponentMenu("MoonAntonio/Saltos/Jugador")]
 	public class Jugador : MonoBehaviour 
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Rigidbody del jugador.</para>
+		/// </summary>
+		public Rigidbody2D rb;											// Rigidbody del jugador
+		#endregion
+
+		#region Actualizadores
+		/// <summary>
+		/// <para>Actualizador de <see cref="Jugador"/>.</para>
+		/// </summary>
+		private void Update()// Actualizador de Jugador
+		{
+			// Control de entrada
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				// Si presionamos flecha derecha -> movemos
+				rb.MovePosition(rb.position + Vector2.right);
+			}
+			else if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				// Si presionamos flecha izquierda -> movemos
+				rb.MovePosition(rb.position + Vector2.left);
+			}
+			else if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				// Si presionamos flecha arriba -> movemos
+				rb.MovePosition(rb.position + Vector2.up);
+			}
+			else if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				// Si presionamos flecha abajo -> movemos
+				rb.MovePosition(rb.position + Vector2.down);
+			}
+
+		}
+		#endregion
 	}
 }

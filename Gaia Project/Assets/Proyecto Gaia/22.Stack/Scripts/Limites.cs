3dbe9de074f611bc298122c9535b07f3ba373035
Moon Antonio <pinchodev91@gmail.com>
2017-06-22 19:41:14 +0200

    Add limites de la escena

---
+++ Gaia Project/Assets/Proyecto Gaia/22.Stack/Scripts/Limites.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Limites.cs (22/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control para los limites de la escena						\\
+// Fecha Mod:		22/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Stack
+{
+	/// <summary>
+	/// <para>Control para los limites de la escena</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Stack/Limites")]
+	public class Limites : MonoBehaviour 
+	{
+
+	}
+}

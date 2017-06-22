eb4e6d97828b4d1856aa0ef2d3342bafab7656c9
Moon Antonio <pinchodev91@gmail.com>
2017-06-22 19:37:42 +0200

    Up

---
+++ Gaia Project/Assets/Proyecto Gaia/22.Stack/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Manager.cs (22/06/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Manager del sistema stack									\\
+// Fecha Mod:		22/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.Stack
+{
+	/// <summary>
+	/// <para>Manager del sistema stack</para>
+	/// </summary>
+	[AddComponentMenu("MoonAntonio/Stack/Manager")]
+	public class Manager : MonoBehaviour 
+	{
+
+	}
+}
